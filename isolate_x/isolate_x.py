b1f31ecc113c8d2e706d9513161709c53a35b1d7
icelal-kskn <programmer.keskincelal@gmail.com>
2024-12-17 14:17:08 +0300

    Update isolate_x.py

--- isolate_x/isolate_x.py
+++ isolate_x/isolate_x.py
@@ -1,45 +1,67 @@
-from sympy import symbols,Eq,sin,cos,tan,sec,csc,cot,Mul
+from sympy import symbols,Eq,sin,cos,tan,sec,csc,cot,asin,acos,atan,acot,log,exp,ln
 
 
-#trigonometric, logarithmic and nested functions are not supported
+#Note that trigonometric functions lose their domain of definition if the inverse is taken. //Write frontend
 def isolate_x(equation):
-    print(f"Given Equation:{equation}")
     left_side = equation.lhs
     right_side = equation.rhs
 
-    highest_degree_term = left_side.as_ordered_terms()[0]
-    right_side = highest_degree_term
-    left_side = highest_degree_term - left_side 
+    x_terms = [i for i in left_side.as_ordered_terms() if i.has(x) ]  
+    right_side = x_terms[0]
+    left_side = x_terms[0] - left_side 
     
+    inverse_map = {
+        sin: asin,
+        cos: acos,
+        tan: atan,
+        cot: acot,
+        sec: lambda x: acos(1/x),  # Special handling for sec
+        csc: lambda x: asin(1/x),   # Special handling for csc
+        asin: sin,
+        acos: cos,
+        atan: tan,
+        acot: cot,
+        log: exp,
+        exp:log
+    }
 
-    while not (right_side.is_Atom and right_side.has(x)):
+    patience = 10
+    while not right_side.is_Atom :
+        patience -=1
+        if patience < 0:
+            print("Not Enough Patience for this equation")
+            break
 
-        if right_side.is_Mul:
+        if right_side.is_Mul: 
             args = right_side.args
-            for i in range(1,len(args)):
-                right_side = right_side / args[i] 
-                left_side = left_side / args[i]
+            if any(arg.is_Rational and arg.denominator == 1 for arg in args): #dividing 
+                left_side = (left_side / right_side.args[0])
+                if left_side != 0:
+                    left_side = 1/left_side
+                right_side, _  = right_side.args[1].as_base_exp()
+
+            else: #multiplication
+                for i in range(1,len(args)):
+                    right_side = right_side / args[i] 
+                    left_side = left_side / args[i]
         
         elif right_side.is_Pow:
             base,exponent = right_side.as_base_exp()
             right_side = base
-            left_side = left_side ** (1 / exponent) 
+            left_side = left_side ** (1 / exponent)
+             
         
-        elif right_side.is_Function:
-            if right_side.func is [sin,cos,tan,cot,sec,csc]:
-                pass
-            # elif right_side.func is [log,ln]:
-            #     pass
+        elif right_side.is_Function: #trigh not supported
+            if right_side.func in inverse_map:  
+                inverse_func = inverse_map[right_side.func]
+                right_side = right_side.args[0]
+                left_side = inverse_func(left_side)
             else:
                 raise NotImplementedError(f"Cannot isolate {right_side}")
 
-            
-        
         else:
             raise NotImplementedError(f"Cannot isolate {right_side}")
-        
-        
-    print(f"Left Side:{left_side} Right Side:{right_side}")
+
 
     return left_side, right_side
 
@@ -47,12 +69,34 @@ def isolate_x(equation):
 if __name__ == "__main__":
     x = symbols('x')
     equations = [
+        "x**2 + 3*x + 3/x",
+        "3 / cos(x)",
+        "3/x",
         "sin(x) * x ** 2 * cos(x) + 4",
         "x**2 + 4*x - 5",
+        "sin(x)+4",
         "sin(cos(x))+cos(sin(x))",
         "cos(x)**2 + 5 ",
-        "sin(cos(x))"
+        "sin(cos(x))",
+        "x**3 - 6*x**2 + 11*x - 6",
+        "tan(x) + 3",
+        "cos(sin(x)) + 1",
+        "exp(2*x) - 5",
+        "log(x) - 4",
+        "log(x,5) - 5",
+        "ln(x)",
+        "ln(x) - 4",
+        "sin(x) * cos(x) - 1/2",
+        "cos(x)**2 - sin(x)**2",
+        "x**2 * tan(x) + x - 3",
+        "sec(x) - 2",
+        "csc(x) + 3"
     ]
     for equation_str in equations: #f(x) = 0
         equation = Eq(eval(equation_str),0)
-        isolate_x(equation)   # g(x)
+        left_side,right_side=isolate_x(equation)   # g(x)
+        if str(right_side) == "x":
+            g = str(left_side)
+            print(f"g_x = {g}")
+        else:
+            print("Did not find the g_x")
